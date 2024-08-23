218c75899f58bfec527207d150e29f6c43b33e46
ranjen_vasudiven <ranjen.vasudiven@smarttradzt.com>
2024-08-23 17:17:38 +0800

    Starting with the Booking Form

--- src/app/bookings/create-booking/create-booking.component.ts
+++ src/app/bookings/create-booking/create-booking.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 
 import { Place } from '../../places/place.model';
