e97e0652765ab052ea283ce8490428e766dc56ea
ranjen_vasudiven <ranjen.vasudiven@smarttradzt.com>
2024-09-06 14:34:45 +0800

    Outputting Address & Map

--- src/app/places/discover/place-detail/place-detail.module.ts
+++ src/app/places/discover/place-detail/place-detail.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 
 import { PlaceDetailPage } from './place-detail.page';
 import { CreateBookingComponent } from '../../../bookings/create-booking/create-booking.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 const routes: Routes = [
   {
@@ -21,6 +22,7 @@ const routes: Routes = [
     FormsModule,
     IonicModule,
     RouterModule.forChild(routes),
+    SharedModule,
   ],
   declarations: [PlaceDetailPage, CreateBookingComponent],
 })
--- src/app/shared/map-modal/map-modal.component.ts
+++ src/app/shared/map-modal/map-modal.component.ts
@@ -5,6 +5,7 @@ import {
   ElementRef,
   Renderer2,
   OnDestroy,
+  Input,
 } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { environment } from '../../../environments/environment';
@@ -16,6 +17,10 @@ import { environment } from '../../../environments/environment';
 })
 export class MapModalComponent implements AfterViewInit, OnDestroy {
   @ViewChild('map', { static: false }) mapElementRef!: ElementRef;
+  @Input() center = { lat: -34.397, lng: 150.644 };
+  @Input() selectable = true;
+  @Input() closeButtonText = 'Cancel';
+  @Input() title = 'Pick Location';
   clickListener: any;
   googleMaps: any;
 
@@ -30,7 +35,7 @@ export class MapModalComponent implements AfterViewInit, OnDestroy {
         this.googleMaps = googleMaps;
         const mapEl = this.mapElementRef.nativeElement;
         const map = new googleMaps.Map(mapEl, {
-          center: { lat: -34.397, lng: 150.644 },
+          center: this.center,
           zoom: 16,
         });
 
@@ -38,16 +43,25 @@ export class MapModalComponent implements AfterViewInit, OnDestroy {
           this.renderer.addClass(mapEl, 'visible');
         });
 
-        this.clickListener = map.addListener(
-          'click',
-          (event: { latLng: { lat: () => any; lng: () => any } }) => {
-            const selectedCoords = {
-              lat: event.latLng.lat(),
-              lng: event.latLng.lng(),
-            };
-            this.modalCtrl.dismiss(selectedCoords);
-          }
-        );
+        if (this.selectable) {
+          this.clickListener = map.addListener(
+            'click',
+            (event: { latLng: { lat: () => any; lng: () => any } }) => {
+              const selectedCoords = {
+                lat: event.latLng.lat(),
+                lng: event.latLng.lng(),
+              };
+              this.modalCtrl.dismiss(selectedCoords);
+            }
+          );
+        } else {
+          const marker = new googleMaps.Marker({
+            position: this.center,
+            map: map,
+            title: 'Picked Location',
+          });
+          marker.setMap(map);
+        }
       })
       .catch((err) => {
         console.log(err);
